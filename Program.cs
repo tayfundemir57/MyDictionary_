7d25889f365060eb39c9180557198844aba26aed
OMEN <OMEN@LAPTOP-DQKA7T18>
2021-02-21 10:16:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MyDictionary_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string> Dersler = new MyDictionary<string>();
+
+            Dersler.Add("Türçe");
+            Dersler.Add("Matematik");
+            Dersler.Add("Kimya");
+            Dersler.Add("Teknik Resim");
+            Console.WriteLine(Dersler.Count);
+        }
+    }
+
+
+    class MyDictionary<T>
+    {
+        T[] _mydictionary;
+        T[] _tempMyDictionary;
+
+        public MyDictionary()
+        {
+            _mydictionary = new T[0];
+
+        }
+
+        public void Add(T item)
+        {
+            _tempMyDictionary = _mydictionary;
+
+            _mydictionary = new T[_mydictionary.Length + 1];
+
+            for (int i = 0; i < _tempMyDictionary.Length; i++)
+            {
+
+                _mydictionary[i] = _tempMyDictionary[i];
+
+            }
+
+            _mydictionary[_mydictionary.Length - 1] = item;
+
+        }
+
+        public int Count
+        {
+
+            get { return _mydictionary.Length; }
+
+        }
+
+
+    }
+
+}
